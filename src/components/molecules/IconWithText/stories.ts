c3bb8a6e946bdd8985618a13691ecbf93da43ac2
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-11-25 15:51:06 +0900

    feat: IconWithText 컴포넌트 개발

---
+++ src/components/molecules/IconWithText/stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { IconWithText } from ".";
+import { LocationIcon } from "components/atoms/Icon";
+
+const meta: Meta<typeof IconWithText> = {
+  title: "Molecules/IconWithText",
+  component: IconWithText,
+  tags: ["autodocs"],
+};
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const defaultState: Story = {
+  args: {
+    icon: LocationIcon,
+    content: "위치",
+    onClick: () => {
+      console.log("defaultState 클릭!");
+    },
+    iconLocation: "default",
+  },
+};
+
+export const descState: Story = {
+  args: {
+    icon: LocationIcon,
+    content: "위치",
+    desc: "어디에서 사용되는지 작성",
+    onClick: () => {
+      console.log("descState 클릭!");
+    },
+    iconLocation: "default",
+  },
+};
---
+++ src/components/molecules/index.ts
@@ -0,0 +1,1 @@
+export { IconWithText } from "./IconWithText";
