a4425a8dafad92382214d5b4c37d81a788d8f5c7
y0unj1NoH <nohyoong711@gmail.com>
2024-11-25 12:13:06 +0900

    feat: TextButton 컴포넌트 개발

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -5,4 +5,5 @@ export { Label } from "./Label";
 export { Logo } from "./Logo";
 export { TabIndicator } from "./TabIndicator";
 export { Text } from "./Text";
+export { TextButton } from "./Button/TextButton";
 export { ToastInstance as Toast } from "./Toast";
