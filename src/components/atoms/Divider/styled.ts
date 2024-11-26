4bfc7782db3983f4f03e3ab822e74635d33eb66a
ppyom <ouob77@kakao.com>
2024-11-26 09:47:17 +0900

    fix: Divider에 적용한 스타일 index.css(reset.css)로 이동

--- src/components/atoms/Divider/styled.ts
+++ src/components/atoms/Divider/styled.ts
@@ -1,5 +1,3 @@
 import styled from "@emotion/styled";
 
-export const DividerWrapper: ReturnType<typeof styled.hr> = styled.hr`
-  margin: 0;
-`;
+export const DividerWrapper: ReturnType<typeof styled.hr> = styled.hr``;
