dcc9243dc673b75725d66f686bf4da1c2af81332
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-11-26 11:52:13 +0900

    chore: day.js 플러그인 사용으로 코드 변경

--- src/utils/dayFormatter.ts
+++ src/utils/dayFormatter.ts
@@ -1,4 +1,14 @@
 import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/ko"; /** 한국어 로케일 임포트 */
+
+/** 플러그인 확장 */
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
+
+/** 로케일 설정 (한국어) */
+dayjs.locale("ko");
 
 /**
  * 현재 시간이랑 param으로 들어온 Date 비교해서 남은 시간을 `XX일 XX시간 XX분 XX초` 형태로 리턴하는 함수입니다.
@@ -13,24 +23,22 @@ export const getTimeRemaining = (targetDate: Date): string => {
     return "마감시간이 지나 경매가 종료되었습니다!";
   }
 
-  const diff = target.diff(now);
-  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-  const minutes = Math.floor((diff / (1000 * 60)) % 60);
-  const seconds = Math.floor((diff / 1000) % 60);
-
+  const diff = dayjs.duration(target.diff(now));
   const parts: string[] = [];
 
-  if (days > 0) parts.push(`${days}일`);
-  if (hours > 0) parts.push(`${hours}시간`);
-  if (minutes > 0) parts.push(`${String(minutes).padStart(2, "0")}분`);
-  if (seconds > 0) parts.push(`${String(seconds).padStart(2, "0")}초`);
+  if (diff.days() > 0) parts.push(`${diff.days()}일`);
+  if (diff.hours() > 0) parts.push(`${diff.hours()}시간`);
+  if (diff.minutes() > 0)
+    parts.push(`${String(diff.minutes()).padStart(2, "0")}분`);
+  if (diff.seconds() > 0)
+    parts.push(`${String(diff.seconds()).padStart(2, "0")}초`);
 
   return parts.join(" ");
 };
 
 /**
  * 현재 시간이랑 param으로 들어온 Date 비교해서 남은 시간을 아래 형태로 변경하는 함수
+ * https://day.js.org/docs/en/durations/humanize 참조 바람
  * 1초 ~ 59초 = "n 초 전",
  * 1분 ~ 59분 = "n 분 전",
  * 1시 ~ 23시간 = "n 시간 전"
@@ -40,32 +48,11 @@ export const getTimeRemaining = (targetDate: Date): string => {
  * @param date Date
  * @returns string
  */
-export const getRelativeTime = (date: string): string => {
+export const getRelativeTime = (date: Date): string => {
   const now = dayjs();
   const target = dayjs(date);
-
   if (target.isAfter(now)) {
     return "전달받은 시간 값이 미래의 시간입니다.";
   }
-
-  const diffInSeconds = now.diff(target, "second");
-  const diffInMinutes = now.diff(target, "minute");
-  const diffInHours = now.diff(target, "hour");
-  const diffInDays = now.diff(target, "day");
-  const diffInMonths = now.diff(target, "month");
-  const diffInYears = now.diff(target, "year");
-
-  if (diffInSeconds < 60) {
-    return `${diffInSeconds}초 전`;
-  } else if (diffInMinutes < 60) {
-    return `${diffInMinutes}분 전`;
-  } else if (diffInHours < 24) {
-    return `${diffInHours}시간 전`;
-  } else if (diffInDays < 30) {
-    return `${diffInDays}일 전`;
-  } else if (diffInDays < 365) {
-    return `${diffInMonths}개월 전`;
-  } else {
-    return `${diffInYears}년 전`;
-  }
+  return dayjs.duration(target.diff(now)).humanize(true);
 };
