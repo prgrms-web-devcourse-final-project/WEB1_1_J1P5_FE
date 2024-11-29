e56dc18a94ea7bae296f1d8eb6c57d9bc397fd89
ppyom <ouob77@kakao.com>
2024-11-29 09:45:02 +0900

    feat: ChatMessages 컴포넌트 개발

---
+++ src/components/organisms/ChatMessages/index.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { InputWithButton } from "components/molecules";
+import { ChatBubble, type IChatBubbleProps } from "../ChatBubble";
+import {
+  ChatBubblesWrapper,
+  ChatMessagesWrapper,
+  WriteBoxWrapper,
+} from "./styled";
+
+interface IComponentProps {
+  chatBubbles: IChatBubbleProps[];
+  onWriteMessage: (message: string) => void;
+}
+
+export const ChatMessages = ({
+  chatBubbles,
+  onWriteMessage,
+}: IComponentProps) => {
+  const [message, setMessage] = useState("");
+  const handleSendButtonClick = () => {
+    if (!message.trim()) {
+      return;
+    }
+    onWriteMessage(message);
+  };
+
+  useEffect(() => {
+    scrollTo({ top: window.innerHeight });
+  }, []);
+
+  return (
+    <ChatMessagesWrapper>
+      <ChatBubblesWrapper>
+        {chatBubbles.map((bubble, idx) => (
+          <ChatBubble key={`chat_bubble_${idx}`} {...bubble} />
+        ))}
+      </ChatBubblesWrapper>
+      <WriteBoxWrapper>
+        <InputWithButton
+          value={message}
+          setValue={setMessage}
+          buttonText="전송"
+          onButtonClick={handleSendButtonClick}
+        />
+      </WriteBoxWrapper>
+    </ChatMessagesWrapper>
+  );
+};
