fde0ccd3a3ec4aaa8bb4550d31c88eff2490625f
y0unj1NoH <nohyoong711@gmail.com>
2024-12-01 22:21:05 +0900

    fix: id, name prop 모두 추가

--- src/components/molecules/LabeledInput/index.tsx
+++ src/components/molecules/LabeledInput/index.tsx
@@ -2,7 +2,7 @@ import { Label, Input } from "components/atoms";
 import { LabeledInputWrapper } from "./styled";
 
 interface ILabeledInputProps {
-  /** Label와 Input의 htmlFor */
+  /** Label의 htmlFor 및 Input의 id */
   id: string;
   /** Label에 들어갈 텍스트 */
   label: string;
@@ -28,6 +28,7 @@ export const LabeledInput = ({
     <Label text={label} htmlFor={id}></Label>
     <Input
       id={id}
+      name={id}
       placeholder={placeholder}
       value={value}
       setValue={setValue}
@@ -35,4 +36,3 @@ export const LabeledInput = ({
     />
   </LabeledInputWrapper>
 );
-
