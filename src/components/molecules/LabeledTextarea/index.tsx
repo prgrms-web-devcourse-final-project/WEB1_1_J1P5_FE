0e1e56ca44b664181ab6e44e75eecc9e83deaa22
y0unj1NoH <nohyoong711@gmail.com>
2024-12-01 22:52:25 +0900

    feat: LabeledTextarea 컴포넌트 개발

---
+++ src/components/molecules/LabeledTextarea/index.tsx
@@ -0,0 +1,34 @@
+import { Label, Textarea } from "components/atoms";
+import { LabeledTextareaWrapper } from "./styled";
+
+interface ILabeledTextareatProps {
+  /** Label의 htmlFor 및 Input의 id */
+  id: string;
+  /** Label에 들어갈 텍스트 */
+  label: string;
+  /** value set 함수 */
+  value: string;
+  /** placeholder */
+  placeholder?: string;
+  /** value set 함수 */
+  setValue?: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const LabeledTextarea = ({
+  id,
+  label,
+  value,
+  placeholder,
+  setValue
+}: ILabeledTextareatProps) => (
+  <LabeledTextareaWrapper>
+    <Label text={label} htmlFor={id}></Label>
+    <Textarea
+      id={id}
+      name={id}
+      placeholder={placeholder}
+      value={value}
+      setValue={setValue}
+    />
+  </LabeledTextareaWrapper>
+);
---
+++ src/components/molecules/LabeledTextarea/styled.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const LabeledTextareaWrapper: ReturnType<typeof styled.div> = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 8px;
+  width: 100%;
+`;
