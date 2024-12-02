32c69e5f613b3f99666eb75508d128e216eb4e49
ppyom <ouob77@kakao.com>
2024-12-02 11:46:55 +0900

    feat: RootLayout 컴포넌트 구현

--- src/components/organisms/BottomNavBar/styled.ts
+++ src/components/organisms/BottomNavBar/styled.ts
@@ -4,6 +4,8 @@ import { NavBarItemWrapper } from "components/molecules/NavBarItem/styled";
 export const BottomNavBarWrapper: ReturnType<typeof styled.div> = styled.div`
   display: flex;
   justify-content: space-between;
+  background-color: #ffffff;
+  padding: 0.25rem 0;
   ${NavBarItemWrapper} {
     flex: 1;
   }
