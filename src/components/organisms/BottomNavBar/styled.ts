8a798bd4c076be3d594055bff0a176b384396711
ppyom <ouob77@kakao.com>
2024-12-02 10:14:50 +0900

    feat: BottomNavBar 수정
    - 각 메뉴에 해당하는 것들을 상수로 관리
    - 스타일 수정

--- src/components/organisms/BottomNavBar/styled.ts
+++ src/components/organisms/BottomNavBar/styled.ts
@@ -1,6 +1,10 @@
 import styled from "@emotion/styled";
+import { NavBarItemWrapper } from "components/molecules/NavBarItem/styled";
 
 export const BottomNavBarWrapper: ReturnType<typeof styled.div> = styled.div`
   display: flex;
-  gap: 8px;
+  justify-content: space-between;
+  ${NavBarItemWrapper} {
+    flex: 1;
+  }
 `;
