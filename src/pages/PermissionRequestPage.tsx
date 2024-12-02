8818c7aa404b73bd8d3fb26f0a9a218118caf443
ppyom <ouob77@kakao.com>
2024-12-02 16:25:17 +0900

    feat: Firebase FCM 연동

--- src/pages/PermissionRequestPage.tsx
+++ src/pages/PermissionRequestPage.tsx
@@ -1,3 +1,29 @@
+import { usePermission } from "../hooks";
+import { useEffect } from "react";
+
 export const PermissionRequestPage = () => {
+  const { permission, handleRequestPermission } = usePermission();
+
+  /**
+   * 권한 요청 함수
+   *   permission = true일 때까지 반복
+   */
+  const requestPermission = () => {
+    handleRequestPermission()
+      .then((result) => {
+        if (!result) {
+          // TODO 권한 관련 메시지 alert 필요!!
+          console.error("권한을 허용해주세요.");
+        }
+      })
+      .catch(console.error);
+  };
+
+  useEffect(() => {
+    // permission = false면 requestPermission 함수 실행
+    if (!permission) {
+      requestPermission();
+    }
+  }, []);
   return <>PermissionRequestPage</>;
 };
