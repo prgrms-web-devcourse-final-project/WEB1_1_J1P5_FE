2f54288c0354611d5ba6b1cdd51eafae0a219f8b
y0unj1NoH <nohyoong711@gmail.com>
2024-12-03 13:22:41 +0900

    feat: NeighborhoodAuthTemplate 컴포넌트 locationErrorEvent prop 추가

--- src/components/templates/NeighborhoodAuthTemplate/stories.tsx
+++ src/components/templates/NeighborhoodAuthTemplate/stories.tsx
@@ -11,12 +11,16 @@ const meta: Meta<typeof NeighborhoodAuthTemplate> = {
     onSubmitButtonClick: {
       action: "onSubmitButtonClick",
       description: "동네 인증 완료 버튼 클릭 이벤트"
+    },
+    locationErrorEvent: {
+      action: "locationErrorEvent",
+      description: "위치 권한 가져오기 실패 시 모달을 실행할 함수"
     }
   },
   decorators: (story) => (
     <NavermapsProvider
       ncpClientId={import.meta.env.VITE_NAVER_MAP_CLIENT_ID}
-      submodules={["reverseGeocode"]}
+      submodules={["geocoder"]}
     >
       {story()}
     </NavermapsProvider>
@@ -26,7 +30,10 @@ const meta: Meta<typeof NeighborhoodAuthTemplate> = {
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: { onSubmitButtonClick: (location: ILocation) => console.log(location) },
+  args: {
+    onSubmitButtonClick: (location: ILocation) => console.log(location),
+    locationErrorEvent: (message: string) => console.log(message)
+  },
   render: (args) => <NeighborhoodAuthTemplate {...args} />
 };
 
