90d3bbae725034fcc211c8a070410423ff3f015e
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-12-04 11:41:24 +0900

    chore: 백엔드 암호화 없는 관계로 암호화 부분 임시 제거

--- src/utils/security.ts
+++ src/utils/security.ts
@@ -17,20 +17,24 @@ const substitutionsBeforeDecryption: Map<string, string> = new Map([
 ]);
 
 export const encryptRoomId = (roomId: string): string => {
-  const secretKey = "YOUR_SECRET_KEY";
-  const encrypted = CryptoJS.AES.encrypt(roomId, secretKey).toString();
+  return roomId;
+  // const secretKey = "YOUR_SECRET_KEY";
+  // const encrypted = CryptoJS.AES.encrypt(roomId, secretKey).toString();
 
-  return encrypted.replace(
-    /[+/=]/g, // Base64의 특수 문자 매칭
-    (match: string) => substitutionsAfterEncryption.get(match) ?? match
-  );
+  // // return encrypted.replace(
+  // //   /[+/=]/g, // Base64의 특수 문자 매칭
+  // //   (match: string) => substitutionsAfterEncryption.get(match) ?? match
+  // // );
 };
 
 export const decryptRoomId = (encrypted: string): string => {
+  return encrypted;
+  /*
   const toDecrypt = encrypted.replace(
     /[-_~]/g, // URL-safe로 변환된 문자 매칭
     (match: string) => substitutionsBeforeDecryption.get(match) ?? match
   );
   const secretKey = "YOUR_SECRET_KEY";
   return CryptoJS.AES.decrypt(toDecrypt, secretKey).toString(Utf8);
+  */
 };
