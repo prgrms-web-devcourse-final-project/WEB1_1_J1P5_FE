ef92f14c1be41c7c71648e148ac4dd3245583c09
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-12-04 14:56:44 +0900

    chore: css 조정 및 채팅쪽 프로필이미지 제대로 안나오던 현상 fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { NavermapsProvider } from "react-naver-maps";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <NavermapsProvider ncpClientId={import.meta.env.VITE_NAVER_MAP_CLIENT_ID}>
-      <App />
-    </NavermapsProvider>
-  </StrictMode>
+  <NavermapsProvider ncpClientId={import.meta.env.VITE_NAVER_MAP_CLIENT_ID}>
+    <App />
+  </NavermapsProvider>
 );
