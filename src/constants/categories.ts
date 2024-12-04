1aaff4a6e87ed53e44eb11fb7c06fc50ee1a34ad
y0unj1NoH <nohyoong711@gmail.com>
2024-12-04 18:34:18 +0900

    fix: 백엔드팀 카테고리로 수정

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -3,7 +3,7 @@ import type { ICategory } from "types";
 /**
  * TODO: 백엔드 데이터 보고 수정 필요
  */
-export const CATEGORIES: ICategory[] = [
+export const CATEGORIES_LEGACY: ICategory[] = [
   { code: "digital_devices", name: "디지털기기" },
   { code: "home_appliances", name: "생활가전" },
   { code: "furniture_interior", name: "가구/인테리어" },
@@ -25,3 +25,20 @@ export const CATEGORIES: ICategory[] = [
   { code: "other_used_goods", name: "기타 중고물품" }
 ];
 
+export const CATEGORIES: ICategory[] = [
+  { code: "ELECTRONIC", name: "전자제품" },
+  { code: "FURNITURE", name: "가구" },
+  { code: "CLOTHING", name: "의류" },
+  { code: "FOOD", name: "식품" },
+  { code: "BEAUTY", name: "뷰티/화장품" },
+  { code: "SPORTS", name: "스포츠 용품" },
+  { code: "TOYS", name: "장난감" },
+  { code: "BOOKS", name: "도서" },
+  { code: "AUTOMOTIVE", name: "자동차 관련 제품" },
+  { code: "JEWELRY", name: "보석류" },
+  { code: "HOME_APPLIANCES", name: "가전제품" },
+  { code: "PET_SUPPLIES", name: "반려동물 용품" },
+  { code: "OFFICE_SUPPLIES", name: "사무용품" },
+  { code: "GARDEN", name: "정원용품" },
+  { code: "MUSIC", name: "음악 관련 제품" }
+];
