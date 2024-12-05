d3ec9f0fb6f9cca022ae42ba5b43f54ecbdb7a8c
y0unj1NoH <nohyoong711@gmail.com>
2024-12-05 20:03:04 +0900

    fix: IBlockedUserItem 수정 및 IProfile 삭제

--- src/components/templates/BlockedUsersTemplate/stories.tsx
+++ src/components/templates/BlockedUsersTemplate/stories.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { BlockedUsersTemplate } from ".";
-import type { IProfile, IBlockedUserItem } from "types";
+import type { IBlockedUserItem } from "types";
 
 const meta: Meta<typeof BlockedUsersTemplate> = {
   title: "Templates/BlockedUsersTemplate",
@@ -14,10 +14,10 @@ type Story = StoryObj<typeof meta>;
 /** 공통 프로필 생성 함수 */
 const createProfile = (
   isBlocked: boolean,
-  memberId= 1,
+  memberId = 1,
   nickname = "11시27분",
   address = "망원동"
-): IProfile => ({
+): IBlockedUserItem => ({
   memberId,
   imgUrl: "https://github.com/ppyom.png",
   nickname,
@@ -27,8 +27,11 @@ const createProfile = (
 
 /**  공통 유저 아이템 생성 함수 */
 const createUserItem = (isBlocked: boolean): IBlockedUserItem => ({
-  profile: createProfile(isBlocked),
-  onClick: () => {}
+  memberId: 1,
+  imgUrl: "https://github.com/ppyom.png",
+  nickname: "11시27분",
+  address: "망원동",
+  isBlocked
 });
 
 /**  공통 유저 리스트 생성 함수 */
@@ -48,23 +51,21 @@ const BlockedUsersTemplateWithState = () => {
         i === index
           ? {
               ...item,
-              profile: {
-                ...item.profile,
-                isBlocked: !item.profile.isBlocked
-              }
+              isBlocked: !item.isBlocked
             }
           : item
       )
     );
   };
 
-  const itemsWithHandlers = blockedUserItems.map((item, index) => ({
-    ...item,
-    onClick: () => toggleBlockStatus(index) // onClick 핸들러 설정
-  }));
-
-  return <BlockedUsersTemplate blockedUserItems={itemsWithHandlers} />;
+  return (
+    <BlockedUsersTemplate
+      blockedUserItems={blockedUserItems}
+      onClick={toggleBlockStatus}
+    />
+  );
 };
+
 export const Default: Story = {
   render: () => <BlockedUsersTemplateWithState />
 };
--- src/types/profile.d.ts
+++ src/types/profile.d.ts
@@ -1,4 +1,4 @@
-export interface IProfile {
+export interface IBlockedUserItem {
   /** 유저 IDd */
   memberId: number;
   /** 프로필 이미지 URL */
@@ -9,10 +9,4 @@ export interface IProfile {
   address: string;
   /** 차단 여부 */
   isBlocked: boolean;
-}
-export interface IBlockedUserItem {
-  /** 유저 프로필 */
-  profile: IProfile;
-  /** 차단하기 버튼 클릭시 동작 */
-  onClick: (index: number) => void;
 }
