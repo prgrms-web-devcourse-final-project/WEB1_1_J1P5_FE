a68f6d156c31c686cea5449ed016a012dee15ee0
y0unj1NoH <nohyoong711@gmail.com>
2024-12-06 17:02:30 +0900

    chore: SelectLocation 페이지 디자인 반영

--- src/components/organisms/LocationPicker/styled.ts
+++ src/components/organisms/LocationPicker/styled.ts
@@ -1,10 +1,14 @@
 import styled from "@emotion/styled";
+import { IconButtonWrapper } from "components/atoms/Button/IconButton/styled";
+import { TextButtonWrapper } from "components/atoms/Button/TextButton/styled";
 import { MapWrapper } from "components/organisms/Map/styled";
 
 export const PaddingWrapper: ReturnType<typeof styled.div> = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 1rem;
   padding: 0 1rem;
+  width: 100%;
 `;
 
 export const LocationPickerWrapper: ReturnType<typeof styled.div> = styled.div`
@@ -13,8 +17,23 @@ export const LocationPickerWrapper: ReturnType<typeof styled.div> = styled.div`
   width: 100%;
   height: 100%;
   justify-content: space-between;
+  gap: 1rem;
+
+  position: relative;
 
   ${MapWrapper} {
-    height: 60vh;
+    flex: 1;
+  }
+
+  ${IconButtonWrapper} {
+    bottom: 6rem;
+    // TODO: 1rem면 왜 사라지는지?
+    right: 1.1rem;
+  }
+
+  ${TextButtonWrapper} {
+    position: absolute;
+    width: calc(100% - 2rem);
+    bottom: 2rem;
   }
 `;
--- src/components/organisms/Map/styled.ts
+++ src/components/organisms/Map/styled.ts
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
 import { IconButtonWrapper } from "components/atoms/Button/IconButton/styled";
-import { TextButtonWrapper } from "components/atoms/Button/TextButton/styled";
 import { ImageWrapper } from "components/atoms/Image/styled";
 
 export const MapWrapper: ReturnType<typeof styled.div> = styled.div`
@@ -14,14 +13,11 @@ export const MapWrapper: ReturnType<typeof styled.div> = styled.div`
     position: absolute;
     bottom: 32px;
     right: 16px;
+    width: 50px;
+    height: 50px;
     background: #fff;
     outline: none;
-    box-shadow: 2px 2px 1px 1px rgba(0, 0, 0, 0.5);
-  }
-
-  ${TextButtonWrapper}{
-    width: 100%;
-    box-shadow: 2px 2px 1px 1px rgba(0, 0, 0, 0.5);
+    box-shadow: 0px 0px 20px rgba(19, 27, 83, 0.15);
   }
 }
 `;
