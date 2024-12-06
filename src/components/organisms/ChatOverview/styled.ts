db9fc6a5b086c9d34057d32bf5ab5a44f9e64fbf
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-12-06 11:50:11 +0900

    chore: ChatList 페이지 empty 상태 페이지 추가

--- src/components/organisms/ChatOverview/styled.ts
+++ src/components/organisms/ChatOverview/styled.ts
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 export const ChatOverviewRootWrapper: ReturnType<
   typeof styled.div
 > = styled.div`
+  height: 100%;
   display: flex;
   flex-direction: column;
 `;
@@ -15,4 +16,6 @@ export const ListWrapper: ReturnType<typeof styled.div> = styled.div`
   margin: 0 1rem;
 `;
 
-export const PanelWrapper: ReturnType<typeof styled.div> = styled.div``;
+export const PanelWrapper: ReturnType<typeof styled.div> = styled.div`
+  flex: 1;
+`;
