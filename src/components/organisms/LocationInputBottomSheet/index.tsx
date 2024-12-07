e60bb0222a4cd3fbc1002094f5c429d313a817af
y0unj1NoH <nohyoong711@gmail.com>
2024-12-07 23:31:19 +0900

    chore: LocationInputBottom 컴포넌트 스타일 수정

--- src/components/organisms/LocationInputBottomSheet/index.tsx
+++ src/components/organisms/LocationInputBottomSheet/index.tsx
@@ -16,16 +16,29 @@ export const LocationInputBottomSheet = ({
   onClose,
   place,
   setPlace,
-  onRegistrationButtonClick,
+  onRegistrationButtonClick
 }: ILocationInputBottomSheetProps) => {
   return (
     <LocationInputBottomSheetWrapper open={open} onClose={onClose}>
-      <Text variant="h5" content="선택한 곳의 장소명을 입력해주세요" />
-      <Input
-        value={place}
-        setValue={setPlace}
-        placeholder="예) 미금역 8번 출구 앞, 역전우동 앞"
-      />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "10px",
+          width: "100%"
+        }}
+      >
+        <Text
+          variant="title_bold"
+          content="선택한 곳의 장소명을 입력해주세요"
+        />
+        <Input
+          value={place}
+          setValue={setPlace}
+          placeholder="예) 미금역 8번 출구 앞, 역전우동 앞"
+        />
+      </div>
       <TextButton text="거래 장소 등록" onClick={onRegistrationButtonClick} />
     </LocationInputBottomSheetWrapper>
   );
