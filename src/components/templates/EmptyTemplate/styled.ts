81c77aa3c0a74b37477986a7cf3874d62017024e
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-12-07 09:07:36 +0900

    chore: EmptyTemplate 디자인 수정

--- src/components/templates/EmptyTemplate/styled.ts
+++ src/components/templates/EmptyTemplate/styled.ts
@@ -1,7 +1,17 @@
 import styled from "@emotion/styled";
 
 export const EmptyTemplateWrapper: ReturnType<typeof styled.div> = styled.div`
+  flex: 1;
   display: flex;
-  background-color: #eeeeee;
+  flex-direction: column;
+  justify-content: space-between;
+  white-space: break-spaces;
+
   height: 100%;
+  padding: 5rem 0 1rem 0;
+
+  .text-con {
+    display: flex;
+    justify-content: center;
+  }
 `;
