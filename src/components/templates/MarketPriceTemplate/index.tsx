ede84ef6448ed18d8e7ca969414697a9ec7dd7a1
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-12-07 11:04:02 +0900

    feat: BottomNavBar 아이콘 변경 및 디자인 적용

--- src/components/templates/MarketPriceTemplate/index.tsx
+++ src/components/templates/MarketPriceTemplate/index.tsx
@@ -1,12 +1,20 @@
 import { MarketPriceTemplateWrapper } from "./styled";
 import { IPost, PostList } from "components/organisms/PostList";
 import { EmptyTemplate } from "../EmptyTemplate";
+import { useHeaderStore } from "stores";
+import { useEffect } from "react";
 
 interface IMarketPriceTemplateProps {
   posts: IPost[];
 }
 
 export const MarketPriceTemplate = ({ posts }: IMarketPriceTemplateProps) => {
+  const { setTitle } = useHeaderStore();
+
+  useEffect(() => {
+    setTitle("시세 조회");
+  }, []);
+
   return (
     <MarketPriceTemplateWrapper>
       <div className="post-con">
