363bd342607e0cdfab4b007e93982ec468f399d7
y0unj1NoH <nohyoong711@gmail.com>
2024-12-07 13:39:16 +0900

    feat: NeighborhoodAuthPage 컴포넌트 개발

--- src/pages/NeighborhoodAuthPage.tsx
+++ src/pages/NeighborhoodAuthPage.tsx
@@ -1,33 +1,78 @@
+import { useCallback, useEffect } from "react";
 import { Toast } from "components/atoms";
 import { NeighborhoodAuthTemplate } from "components/templates";
 import { useLocationErrorEvent } from "hooks";
-import { useCallback } from "react";
 import { ILocation, IResponse } from "types";
 import { http } from "services/api";
+import { useActivityAreaStore, useTopBarStore } from "stores";
+import { useNavigate } from "react-router-dom";
 
-/**
- * 사용자의 위치 정보를 서버에 전송하는 함수
- * @param location 사용자의 위치 정보
- */
-const submitUserLocation = async (location: ILocation): Promise<void> => {
-  try {
-    await http.post<IResponse, ILocation>("/user/location", location);
-    Toast.show("동네 인증이 완료되었습니다.", 2000);
-  } catch (error) {
-    console.error("Failed to submit user location:", error);
-    Toast.show("동네 인증에 실패했습니다. 다시 시도해주세요.", 2000);
-  }
-};
+interface IUserLocationPost {
+  latitude: number;
+  longitude: number;
+  emdId: number;
+}
+
+export interface ILocationResponse extends IResponse {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  result: {};
+}
 
 export const NeighborhoodAuthPage = () => {
+  const navigate = useNavigate();
+  const { setTitle } = useTopBarStore();
+  const activityArea = useActivityAreaStore((state) => state.activityArea);
+  const activityAreaId = useActivityAreaStore((state) => state.activityAreaId);
+
+  // TODO: nickname 주스탄드로 가져와야 함 or 프로필 패치
+  const nickname = "알비노 라쿤";
+
+  useEffect(() => {
+    setTitle("동네 인증");
+  }, [setTitle]);
+
+  /**
+   * 사용자의 위치 정보를 서버에 전송하는 함수
+   * @param location 사용자의 위치 정보
+   */
+  const submitUserLocation = useCallback(
+    async (location: ILocation): Promise<void> => {
+      const locationPost: IUserLocationPost = {
+        latitude: location.coord!.lat,
+        longitude: location.coord!.lng,
+        emdId: activityAreaId
+      };
+      try {
+        await http.post<ILocationResponse, IUserLocationPost>(
+          "/user/location",
+          locationPost
+        );
+        // TODO: navigate("/my-page") 중복 제거 필요
+        navigate("/my-page");
+        Toast.show("동네 인증이 완료되었습니다.", 2000);
+      } catch (error) {
+        navigate("/my-page");
+        Toast.show("동네 인증에 실패했습니다. 다시 시도해주세요.", 2000);
+        console.error("Failed to submit user location:", error);
+      }
+    },
+    [activityAreaId, navigate]
+  );
+
   const locationErrorEvent = useLocationErrorEvent();
-  const onSubmitButtonClick = useCallback(async (location: ILocation) => {
-    await submitUserLocation(location);
-  }, []);
+
+  const handleSubmitButtonClick = useCallback(
+    async (location: ILocation) => {
+      await submitUserLocation(location);
+    },
+    [submitUserLocation]
+  );
 
   return (
     <NeighborhoodAuthTemplate
-      onSubmitButtonClick={onSubmitButtonClick}
+      nickname={nickname}
+      myAddress={activityArea}
+      onSubmitButtonClick={handleSubmitButtonClick}
       locationErrorEvent={locationErrorEvent}
     />
   );
