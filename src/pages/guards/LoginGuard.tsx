0d25a22fa797fc96ee8aee4473f6e1c486cff4fc
ppyom <ouob77@kakao.com>
2024-12-07 21:58:23 +0900

    fix: LoginGuard에서도 setUser를 하도록 수정

--- src/pages/guards/LoginGuard.tsx
+++ src/pages/guards/LoginGuard.tsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useUserStore } from "stores";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { getUserProfile } from "services/apis";
 
 interface ILoginGuardProps {
@@ -11,15 +11,23 @@ interface ILoginGuardProps {
  * 로그인한 사용자가 로그인 페이지 접근 시 처리
  */
 export const LoginGuard = ({ children }: ILoginGuardProps) => {
-  const { user } = useUserStore();
+  const { user, setUser } = useUserStore();
 
   useEffect(() => {
-    getUserProfile().then(console.log).catch(console.error);
+    getUserProfile()
+      .then(({ result }) => {
+        setUser({
+          nickname: result.nickname || undefined,
+          profile: result.imageUrl || undefined,
+          emdName: result.activityEmdName || undefined,
+        });
+      })
+      .catch(console.error);
   }, []);
 
   if (user) {
     return <Navigate to="/" />;
   }
 
-  return <>{children}</>;
+  return <Suspense fallback={null}>{children}</Suspense>;
 };
